79101f216b9f25423d858b6d0fdda0ca661b0551
Ömer Taha Öztop <omertahaoztop@hotmail.com>
2020-09-26 15:17:09 +0300

    "Proc"

--- Bloklar, proc ve lambda/Proc.rb
+++ Bloklar, proc ve lambda/Proc.rb
@@ -1,1 +1,13 @@
+yeni_metot= Proc.new do |isim|
+  puts "Merhaba #{isim}"
+end
+yeni_metot.call "Ömer"
 
+
+proc_nesnesi = proc {puts "Merhaba "}
+proc_nesnesi.call
+
+proc_nesnesi=proc do
+  puts "Ruby Öğreniyorum"
+end
+proc_nesnesi.call
